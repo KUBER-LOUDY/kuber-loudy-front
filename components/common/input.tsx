63f770eff3f4344f0178c3bdbd1d0bd31c0e03d0
yhjune <yhjune09@gmail.com>
2023-08-08 23:30:16 +0900

    ✏️ (fix): Add margin x into <input>

--- components/common/input.tsx
+++ components/common/input.tsx
@@ -2,7 +2,7 @@ const LoginInput = () => {
   return (
     <input
       className="
-      text-center w-full py-3 my-2 text-sm font-medium leading-4 rounded-md border border-gray-300
+      text-left w-full px-3 py-3 my-2 text-sm font-medium leading-4 rounded-md border border-gray-300
     bg-white
     focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2"
     ></input>
