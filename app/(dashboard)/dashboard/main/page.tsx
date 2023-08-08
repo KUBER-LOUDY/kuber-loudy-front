8b23e2f758bb73c291ce5a73706303b7e3e2a7a5
HyoJung Yoon <yhjune09@gmail.com>
2023-08-08 20:57:04 +0900

    💄 Add pages

--- app/(dashboard)/dashboard/main/page.tsx
+++ app/(dashboard)/dashboard/main/page.tsx
@@ -1,5 +1,7 @@
-export default function dashboardPage() {
-    return 
+export default function DashboardPage() {
+    return (
+      <h1 className="text-3xl text-black pb-6">Dashboard</h1>
+    )
   }
 
-dashboardPage
+DashboardPage
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next"
 import "@/app/globals.css";
-import { Html } from "next/document";
 
 export const metadata: Metadata ={
     viewport : 'width=device-width, initial-scale=1.0',
@@ -17,40 +16,32 @@ export default function DashboardLayout({
         <body className="bg-gray-100 flex flex-row">
             <aside className="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl ">
                 <div className="p-6">
-                    <a href="dashboard/blank.tsx" className="text-white text-3xl font-semibold uppercase hover:text-gray-300">admin</a>
+                    <a href="/iam/list" className="text-white text-3xl font-semibold uppercase hover:text-gray-300">admin</a>
                         <button className="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
                             <i className="fas fa-plus mr-3"></i>새 Iam 계정
                         </button>
                 </div>
                     <nav className="text-white text-base font-semibold pt-3">
-                        <a href="index.html" className="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
+                        <a href="/dashboard/main" className="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
                             <i className="fas fa-tachometer-alt mr-3"></i>
-                            Dashboard
+                            대쉬보드
                         </a>
-                        <a href="blank.html" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                        <a href="/dashboard/ec2" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                             <i className="fas fa-sticky-note mr-3"></i>
-                            Blank Page
+                            ec2 상태
                         </a>
-                        <a href="tables.html" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                            <i className="fas fa-table mr-3"></i>
-                            Tables
-                        </a>
-                        <a href="forms.html" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                        <a href="/dashboard/kuber" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                             <i className="fas fa-align-left mr-3"></i>
-                            Forms
+                            Kubernetes 상태
                         </a>
-                        <a href="tabs.html" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                        <a href="/dashboard/tutorials" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                             <i className="fas fa-tablet-alt mr-3"></i>
-                            Tabbed Content
-                        </a>
-                        <a href="calendar.html" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                            <i className="fas fa-calendar mr-3"></i>
-                            Calendar
+                            Tutorials
                         </a>
                     </nav>
-                        <a href="#" className="absolute w-full upgrade-btn bottom-0 active-nav-link text-white flex items-center justify-center py-4">
+                        <a href="https://accordions.co.kr" className="absolute w-full upgrade-btn bottom-0 active-nav-link text-white flex items-center justify-center py-4">
                         <i className="fas fa-arrow-circle-up mr-3"></i>
-                        Upgrade to Pro!
+                        Upgrade to Accordian!
                         </a>
             </aside>
                 
@@ -67,7 +58,7 @@ export default function DashboardLayout({
 
                 <div className="w-full overflow-x-hidden border-t flex flex-col">
                     <main className="w-full flex-grow p-6">
-                        <h1 className="text-3xl text-black pb-6">Dashboard</h1>
+                        
 
                         {children}
 
